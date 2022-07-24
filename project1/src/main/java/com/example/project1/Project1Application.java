e671453c249c345301102d7dbd5c057c75185d4c
fb991108 <16607248794@163.com>
2022-07-24 16:32:29 +0800

    feat：更新3

--- project1/src/main/java/com/example/project1/Project1Application.java
+++ project1/src/main/java/com/example/project1/Project1Application.java
@@ -8,6 +8,8 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 public class Project1Application {
 
+
+
     public static void main(String[] args) {
         SpringApplication.run(Project1Application.class, args);
     }
